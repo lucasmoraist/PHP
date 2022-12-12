7f5eda26fd25f6e0884c2a82b2d9ddf1a5897854
lucasmoraisnt <rm93267@fiap.com.br>
2022-12-12 09:24:43 -0300

    Primeiros passos no curso de php. Criando tag para acrescentar o php dentro do html

---
+++ olamundo/ola.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Primeiro exemplo PHP</title>
+</head>
+<body>
+    <h2>Testando PHP</h2>
+    <?php
+        echo "<h2>Olá mundo!</h2>";
+
+    ?>
+</body>
+</html>
