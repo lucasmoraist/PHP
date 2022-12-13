d658fda9126f50a979075ffdda2e1dae6725dca3
lucasmoraisnt <rm93267@fiap.com.br>
2022-12-13 08:59:06 -0300

    Definindo variaveis e vendo como utilizar a saída delas

--- olamundo/ola.php
+++ olamundo/ola.php
@@ -3,11 +3,25 @@
 <head>
     <meta charset="UTF-8"/>
     <title>Primeiro exemplo PHP</title>
+    <style>
+        h2{
+            color: #c0c;
+        }
+    </style>
 </head>
 <body>
-    <h2>Testando PHP</h2>
+    <h1>Testando PHP</h1>
     <?php
         echo "<h2>Olá mundo!</h2>";
+        $idade = 18;
+        $preco = 10.50;
+        $nome = "Lucas de Morais Nascimento Taguchi";
+        $casado = false;
+
+
+
+
+
 
     ?>
 </body>
---
+++ olamundo/variaveis.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis</title>
+</head>
+<body>
+    <h1>Aula sobre variaveis</h1>
+    <?php
+        $idade = 18;
+        $preco = 10.50;
+        $nome = (string)"Lucas de Morais Nascimento Taguchi";
+        $casado = true;
+
+        echo "$nome <br> $idade <br> $casado <br> $preco";
+    ?>
+</body>
+</html>
